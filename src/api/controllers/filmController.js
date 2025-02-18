27583b3a0902c1342a4e501c524d4ad9e7769a48
laurent <email.laurent@gmail.com>
2025-02-18 11:38:17 +0100

    refactor: api for slider

--- src/api/controllers/filmController.js
+++ src/api/controllers/filmController.js
@@ -131,98 +131,9 @@ const getFilmsByDate = async (req, res) => {
   }
 };
 
-const getScreeningsByFilmId = async (req, res) => {
-  const filmId = req.params.id;
-  try {
-    const screeningsByFilmRows = await dbService.query(
-      `SELECT 
-        f.id AS film_id, 
-        f.title, 
-        f.description, 
-        f.release_date, 
-        f.age_minimum, 
-        f.favorite, 
-        f.poster, 
-        s.id AS screening_id, 
-        s.auditorium_id, 
-        s.remaining_seat, 
-        s.remaining_handi_seat, 
-        s.start_time, 
-        s.end_time, 
-        a.name AS auditorium_name, 
-        a.seat AS auditorium_seat, 
-        a.handi_seat AS auditorium_handi_seat,
-        a.cinema_id AS auditorium_cinema_id,
-        q.name AS auditorium_quality,
-        q.price AS auditorium_price
-      FROM 
-        film f 
-        INNER JOIN screening s ON f.id = s.film_id 
-        INNER JOIN auditorium a ON s.auditorium_id = a.id
-        INNER JOIN quality q ON q.id = a.quality_id
-      WHERE 
-        f.id =  ?`,
-      [filmId]
-    );
-
-    if (screeningsByFilmRows.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "Film ou séances non trouvé(es)" });
-    }
-
-    var genres = await dbService.query(
-      `SELECT GROUP_CONCAT(g.name SEPARATOR ', ') AS genres 
-      FROM film f 
-      LEFT JOIN film_genre fg ON f.id = fg.film_id 
-      LEFT JOIN genre g ON fg.genre_id = g.id 
-      WHERE f.id = ?`,
-      [filmId]
-    );
-
-    if (genres.length === 0) {
-      return res.status(404).json({ message: "genres du film non trouvé" });
-    }
-
-    const film = {
-      id: screeningsByFilmRows[0].film_id,
-      title: screeningsByFilmRows[0].title,
-      genres: genres[0].genres,
-      description: screeningsByFilmRows[0].description,
-      release_date: screeningsByFilmRows[0].release_date,
-      age_minimum: screeningsByFilmRows[0].age_minimum,
-      favorite: screeningsByFilmRows[0].favorite,
-      poster: screeningsByFilmRows[0].poster,
-    };
-
-    const screenings = screeningsByFilmRows.map((row) => ({
-      id: row.screening_id,
-      start_time: row.start_time,
-      end_time: row.end_time,
-      auditorium_name: row.auditorium_name,
-      auditorium_seat: row.auditorium_seat,
-      auditorium_handi_seat: row.auditorium_handi_seat,
-      auditorium_cinema_id: row.auditorium_cinema_id,
-      auditorium_quality: row.auditorium_quality,
-      auditorium_price: row.auditorium_price,
-    }));
-
-    res.json({
-      film,
-      screenings,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Erreur lors de la récupération des screenings",
-      error: error.message,
-    });
-  }
-};
-
 module.exports = {
   getFilms,
   getFilmsByCinemaId,
   getFilmsByGenreId,
   getFilmsByDate,
-  getScreeningsByFilmId,
 };
