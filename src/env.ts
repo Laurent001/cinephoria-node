76607cbc0940e3ce54ca6c2b275b3883fb7ca165
laurent <email.laurent@gmail.com>
2025-05-17 20:15:29 +0200

    fix: load dotenv if not in render server

--- src/env.ts
+++ src/env.ts
@@ -1,17 +1,27 @@
 import dotenv from "dotenv";
 import path from "path";
 
-const envPath =
-  process.env.NODE_ENV === "production"
-    ? path.resolve(process.cwd(), ".env_prod")
-    : path.resolve(process.cwd(), ".env");
+const isRender = !!process.env.RENDER;
 
-const result = dotenv.config({ path: envPath });
+if (!isRender) {
+  const envPath =
+    process.env.NODE_ENV === "production"
+      ? path.resolve(process.cwd(), ".env_prod")
+      : path.resolve(process.cwd(), ".env");
 
-if (result.error) {
-  console.warn(`Impossible de charger le fichier d'environnement à ${envPath}`);
+  const result = dotenv.config({ path: envPath });
+
+  if (result.error) {
+    console.warn(
+      `Impossible de charger le fichier d'environnement à ${envPath}`
+    );
+  } else {
+    console.log(`Variables d'environnement chargées depuis : ${envPath}`);
+  }
 } else {
-  console.log(`Variables d'environnement chargées depuis : ${envPath}`);
+  console.log(
+    "Environnement Render détecté : les variables sont injectées par la plateforme."
+  );
 }
 
 export {};
